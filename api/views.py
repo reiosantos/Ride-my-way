08588d1f3185293f04da7a5f98e5003c31e184d2
reiosantos <ronireiosantos@gmail.com>
2018-06-20 23:07:50 +0300

    [#158476979]add feature for deleting a ride offer

--- api/views.py
+++ api/views.py
@@ -6,8 +6,10 @@
 
     :copyright: © 2018 by reio santos.
 """
-
+from flask import request, jsonify
 from flask.views import MethodView
+
+from api.utils import Utils
 
 
 class Rides(MethodView):
@@ -24,3 +26,43 @@ class Rides(MethodView):
 
            app.add_url_rule('/api/v1/rides/', view_func=Rides.as_view('get_rides'))
     """
+    rides = [
+        {"post_date": Utils.make_date_time(), "driver": "Reio", "driver_contact": "0779104144",
+         "trip_to": "nakasongola", "cost": 2000, "status": "taken", "taken_by": "ssekitto",
+         "ride_id": 1, "requested": True, "Requested_by": "ssekitto"},
+        {"post_date": Utils.make_date_time(), "driver": "Santos", "driver_contact": "0779104144",
+         "trip_to": "namasagali", "cost": 12000, "status": "available", "taken_by": None,
+         "ride_id": 2, "requested": False, "requested_by": None},
+        {"post_date": Utils.make_date_time(), "driver": "Ronald", "driver_contact": "0779104144",
+         "trip_to": "nansana", "cost": 5000, "status": "available", "taken_by": None,
+         "ride_id": 3, "requested": False, "requested_by": None},
+    ]
+
+    def delete(self, ride_id):
+        """
+        responds to update requests
+        :return:
+        """
+        if not request or not ride_id:
+            return jsonify({"error_message": "URL is invalid. Ride id is missing a value",
+                            "data": str(request.url_rule)}), 400
+        if str(request.url_rule) == "/api/v1/rides/delete/<int:ride_id>/":
+
+            ride_index = 0
+            exists = False
+            for ride in self.rides:
+                if ride['ride_id'] == ride_id:
+                    exists = True
+                    break
+                ride_index += 1
+
+            if not exists:
+                return jsonify({"error_message": "The requested ride {0} is not found"
+                                                 .format(ride_id),
+                                "data": False}), 404
+
+            self.rides.remove(self.rides[ride_index])
+
+            return jsonify({"success_message": "Ride has been deleted.", "data": True})
+
+        return jsonify({"error_message": "Request could not be processed.", "data": False}), 204
