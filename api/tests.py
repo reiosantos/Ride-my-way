ca0f508704820cb1791b69ed6aee1636d177b4d0
reiosantos <ronireiosantos@gmail.com>
2018-06-20 16:09:09 +0300

    [#158473853]add get ride offers feature

--- api/tests.py
+++ api/tests.py
@@ -13,3 +13,15 @@ class TestClass(TestCase):
     def setUp(self):
         self.app = APP
         self.client = self.app.test_client
+
+    def test_get_rides(self):
+        """
+        Test case for get rides endpoint, it gets all rides
+        """
+        res = self.client().get('/api/v1/rides/')
+        self.assertEqual(res.status_code, 200)
+        self.assertNotIn("data", res.json)
+        self.assertIn("data", res.json)
+        self.assertIsInstance(res.json['data'], list)
+        self.assertTrue(res.json["data"])
+        self.assertIsInstance(res.json["data"][0], dict)
