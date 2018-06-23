f0bef98fa4d40068003da25f1aa93b607d891543
reiosantos <ronireiosantos@gmail.com>
2018-06-23 15:41:12 +0300

    change port numberto 5000

--- api/config.py
+++ api/config.py
@@ -9,7 +9,7 @@ class Config:
     They can be changed at any time.
     """
     HOST = "0.0.0.0"
-    PORT = "5001"
+    PORT = 5000
 
     SECRET_KEY = 'e5ac358c-f0bf-11e5-9e39-d3b532c10a28'
     SECURITY_PASSWORD_SALT = 'efa27950565790fbaecfb5fb64b84a6a7c48d06d'
