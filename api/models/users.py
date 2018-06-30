900148b106fd7b185347a3e68ec7bb2d02d6f51e
reiosantos <ronireiosantos@gmail.com>
2018-06-30 20:07:06 +0300

    add more user methods

--- api/models/users.py
+++ api/models/users.py
@@ -1,13 +1,72 @@
+from typing import List
 
-from api.utils import Utils, JSONSerializable
+from api.utils.utils import JSONSerializable, Utils
 
 
-class UserModel(JSONSerializable):
-    def __init__(self, user_id=1, full_name=None, contact=0, username=None, password=None, user_type=0):
-        self.user_id = user_id
-        self.full_name = full_name
-        self.username = username
-        self.contact = contact
-        self.registration_date = Utils.make_date_time()
-        self.password = password
-        self.user_type = user_type
+class Users:
+    class UserModel(JSONSerializable):
+        def __init__(self, full_name=None, contact=None, username=None,
+                     password=None, user_type=None):
+            self.user_id = Utils.generate_user_id()
+            self.registration_date = Utils.make_date_time()
+
+            self.full_name = full_name
+            self.username = username
+            self.contact = contact
+            self.password = password
+            self.user_type = user_type
+
+    users: List[UserModel] = []
+
+    @staticmethod
+    def create_user(full_name=None, contact=None, username=None,
+                    password=None, user_type="passenger") -> UserModel or None:
+
+        user = Users.UserModel(full_name, contact, username, password, user_type)
+        Users.users.append(user)
+        return user
+
+    @classmethod
+    def get_all_users(cls) -> List[UserModel]:
+        return cls.users
+
+    @classmethod
+    def find_user(cls, user_id) -> UserModel or None:
+        try:
+            user_id = int(user_id)
+        except ValueError:
+            return None
+
+        for user in cls.users:
+            if user.user_id == user_id:
+                return user
+        return None
+
+    @classmethod
+    def update_user(cls, user_id=None, full_name=None, contact=None, username=None,
+                    password=None, user_type="passenger") -> bool:
+
+        user = cls.find_user(user_id)
+        if not user:
+            return False
+        index = cls.users.index(user)
+
+        user.user_type = user_type
+        if password:
+            user.password = password
+        user.full_name = full_name
+        user.contact = contact
+        user.username = username
+
+        cls.users.insert(index, user)
+        return True
+
+    @classmethod
+    def delete_user(cls, user_id) -> bool:
+
+        user = cls.find_user(user_id)
+        if not user:
+            return False
+
+        cls.users.remove(user)
+        return True
